693b1654c02bd4b1811ebc9a5a2efb91037adb26
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-06 14:24:53 +0200

    update

--- src/components/home/MultiCity.tsx
+++ src/components/home/MultiCity.tsx
@@ -1,3 +1,9 @@
 export default function MultiCity() {
-  return <div className="multi-city">MultiCity</div>;
+  return (
+    <div className="multi-city">
+      <div className="multi-title">
+        <h4>Trip</h4>
+      </div>
+    </div>
+  );
 }
