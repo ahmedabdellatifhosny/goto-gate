b7b632fd099b4cf6402ca386baeadc11f9c63851
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-06 15:27:41 +0200

    update add trip

--- src/components/home/MultiCity.tsx
+++ src/components/home/MultiCity.tsx
@@ -1,9 +1,11 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { useState } from "react";
 import Select from "react-select";
 import { Airports, Passengers, Class } from "./data";
 import dynamic from "next/dynamic";
 import DatePicker from "react-datepicker";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function MultiCity() {
   interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -22,15 +24,15 @@ export default function MultiCity() {
   const Select = dynamic(() => import("react-select"), { ssr: false });
   return (
     <div className="multi-city">
-      <div className="multi-title">
-        <h4>Trip</h4>
-      </div>
       <div className="trips">
         <Container>
+          <div className="multi-title">
+            <h5>Trip</h5>
+          </div>
           <Row>
             <Col md={4}>
               <div className="from">
-              <label>From</label>
+                <label>From</label>
                 <Select
                   className="basic-single"
                   classNamePrefix="select"
@@ -43,7 +45,7 @@ export default function MultiCity() {
             </Col>
             <Col md={4}>
               <div className="to">
-              <label>to</label>
+                <label>to</label>
                 <Select
                   className="basic-single"
                   classNamePrefix="select"
@@ -72,10 +74,13 @@ export default function MultiCity() {
       </div>
       <div className="trips">
         <Container>
+          <div className="multi-title">
+            <h5>Trip</h5>
+          </div>
           <Row>
             <Col md={4}>
               <div className="from">
-              <label>From</label>
+                <label>From</label>
                 <Select
                   className="basic-single"
                   classNamePrefix="select"
@@ -88,7 +93,7 @@ export default function MultiCity() {
             </Col>
             <Col md={4}>
               <div className="to">
-              <label>to</label>
+                <label>to</label>
                 <Select
                   className="basic-single"
                   classNamePrefix="select"
@@ -113,6 +118,68 @@ export default function MultiCity() {
               </div>
             </Col>
           </Row>
+          <div className="add-trip">
+            <Button variant="dark" className="text-capitalize">
+              <FontAwesomeIcon icon={faPlus} /> <span> add trip </span>
+            </Button>
+          </div>
+        </Container>
+      </div>
+      <div className="after-add-trip">
+        <Container>
+        <div className="trips">
+        <Container>
+          <div className="multi-title">
+            <h5>Trip</h5>
+          </div>
+          <Row>
+            <Col md={4}>
+              <div className="from">
+                <label>From</label>
+                <Select
+                  className="basic-single"
+                  classNamePrefix="select"
+                  isClearable={isClearable}
+                  name="color"
+                  options={Airports}
+                  placeholder="From"
+                />
+              </div>
+            </Col>
+            <Col md={4}>
+              <div className="to">
+                <label>to</label>
+                <Select
+                  className="basic-single"
+                  classNamePrefix="select"
+                  isClearable={isClearable}
+                  name="color"
+                  options={Airports}
+                  placeholder="To"
+                />
+              </div>
+            </Col>
+            <Col md={4}>
+              <div className="dates d-flex">
+                <div className="departure">
+                  <label>Departure</label>
+                  <DatePicker
+                    dateFormat="yyyy/MM/dd"
+                    selected={startDate}
+                    onChange={(date: Date | null) => setStartDate(date)}
+                    className="start-date form-control"
+                  />
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <div className="add-trip">
+            <Button variant="dark" className="text-capitalize">
+              <FontAwesomeIcon icon={faPlus} /> <span> add trip </span>
+            </Button>
+          </div>
+        </Container>
+      </div>
         </Container>
       </div>
     </div>
