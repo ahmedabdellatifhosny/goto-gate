1e28619d6b3dc99b5306803296eaba2741e47666
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-13 17:52:55 +0200

    update

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -127,18 +127,65 @@ export default function Tickets() {
                     key={placement}
                     placement={placement}
                     overlay={
-                      <Tooltip id={`tooltip-${placement}`}>
-                        Tooltip on {placement}
+                      <Tooltip>
+                        <div className="baggage">
+                          <ul className="list-unstyled">
+                            <li>
+                              <h5>Baggage</h5>
+                              <small>per person</small>
+                            </li>
+                            <li>
+                              <h6>not possible to add with us </h6>
+                            </li>
+                            <li>
+                              <p>
+                                diffrent baggage allawance may apply we show the
+                                minimun allowed. see itinerary for more baggage
+                                details .
+                              </p>
+                            </li>
+                          </ul>
+                        </div>
                       </Tooltip>
                     }
                   >
                     <Button variant="secondary" className="w-100">
-                      Tooltip on {placement}
+                      <ul className="list-unstyled d-flex gap-2 mb-0">
+                        <li>
+                          <span className="me-1">0</span>
+                          <Image
+                            src="/images/icons/bag.svg"
+                            width={20}
+                            height={20}
+                            alt="bag"
+                          />
+                        </li>
+                        <li>
+                          <span className="me-1">1</span>
+                          <Image
+                            src="/images/icons/lakage.svg"
+                            width={20}
+                            height={20}
+                            alt="bag"
+                          />
+                        </li>
+                        <li>
+                          <span className="me-1">0</span>
+                          <Image
+                            src="/images/icons/backbag.svg"
+                            width={20}
+                            height={20}
+                            alt="bag"
+                          />
+                        </li>
+                      </ul>
                     </Button>
                   </OverlayTrigger>
                 ))}
 
-                <div className="hurry py-3 fw-bold">Hurry! Only 1 seat at this price!</div>
+                <div className="hurry py-3 fw-bold">
+                  Hurry! Only 1 seat at this price!
+                </div>
                 <div className="price py-3">
                   <p className="fw-bold fs-3 mb-0 ">US$2,019.10</p>
                   <p className="fs-6 text-end pe-2">Price per adult</p>
