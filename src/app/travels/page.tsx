9567b35140ed250afd301b8c680337dcb1171f1e
ahmedabdellatif <ahmedabdellatifhosny@gmaol.com>
2025-02-13 14:01:47 +0200

    update serch box result

--- src/app/travels/page.tsx
+++ src/app/travels/page.tsx
@@ -1,16 +1,37 @@
 "use client";
+
 import { Container, Row, Col, Button, Collapse } from "react-bootstrap";
 import Sidebar from "../../components/layout/Sidebar";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import { JSX, useState } from "react";
+import { JSX, SetStateAction, useState } from "react";
 import Image from "next/image";
 import { faCalendarDays, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
+import MultiCity from "../../components/home/MultiCity";
+import OneWay from "../../components/home/OneWay";
+import Return from "../../components/home/Return";
+import dynamic from "next/dynamic";
 
 export default function Page(): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
+const [activeTab, setActiveTab] = useState("return");
+  const Select = dynamic(() => import("react-select"), { ssr: false });
+  const handleTabChange = (tab: SetStateAction<string>) => {
+    setActiveTab(tab);
+  };
+
+  interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    children: React.ReactNode;
+  }
+
+  const Checkbox: React.FC<CheckboxProps> = ({ children, ...props }) => (
+    <label>
+      <input type="checkbox" {...props} />
+      {children}
+    </label>
+  );
 
   return (
     <section className="travels">
@@ -39,7 +60,10 @@ export default function Page(): JSX.Element {
               <FontAwesomeIcon icon={faUser} /> <span>1</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faChevronUp} />
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                className={`chevron-icon ${open ? "rotate" : ""}`}
+              />
             </li>
           </ul>
         </Button>
@@ -48,10 +72,83 @@ export default function Page(): JSX.Element {
         <Container>
           <Collapse in={open} className="py-2">
             <div id="example-collapse-text">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident.
+              <div className="flight-search-box rounded">
+                <Tabs
+                  activeKey={activeTab}
+                  onSelect={(tab) => setActiveTab(tab)}
+                  id="radio-tabs-example"
+                  className="py-3"
+                >
+                  <Tab
+                    eventKey="return"
+                    title={
+                      <label className="d-flex align-items-center ">
+                        <input
+                          type="radio"
+                          name="tab"
+                          value="return"
+                          checked={activeTab === "return"}
+                          onChange={() => handleTabChange("return")}
+                          className="me-2"
+                        />
+                        Return
+                      </label>
+                    }
+                  >
+                    <div className="tab-content">
+                      <div className="return">
+                        <Return />
+                      </div>
+                    </div>
+                  </Tab>
+
+                  <Tab
+                    eventKey="oneway"
+                    title={
+                      <label className="d-flex align-items-center">
+                        <input
+                          type="radio"
+                          name="tab"
+                          value="oneway"
+                          checked={activeTab === "oneway"}
+                          onChange={() => handleTabChange("oneway")}
+                          className="me-2"
+                        />
+                        One-Way
+                      </label>
+                    }
+                  >
+                    <div className="tab-content">
+                      <div className="one-way">
+                        <OneWay />
+                      </div>
+                    </div>
+                  </Tab>
+
+                  <Tab
+                    eventKey="multicity"
+                    title={
+                      <label className="d-flex align-items-center">
+                        <input
+                          type="radio"
+                          name="tab"
+                          value="multicity"
+                          checked={activeTab === "multicity"}
+                          onChange={() => handleTabChange("multicity")}
+                          className="me-2"
+                        />
+                        Multi-City
+                      </label>
+                    }
+                  >
+                    <div className="tab-content">
+                      <div className="multi-city">
+                        <MultiCity />
+                      </div>
+                    </div>
+                  </Tab>
+                </Tabs>
+              </div>
             </div>
           </Collapse>
           <Row>
